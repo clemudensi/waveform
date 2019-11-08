bdd7d9e6bc458837a43ba1fa1a858c3a676856a2
clemudensi <cklemmzy@gmail.com>
2019-11-08 06:00:28 +0100

    add waveform to audio player

--- test/src/AudioPlayer.js
+++ test/src/AudioPlayer.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import ReactHowler from 'react-howler';
 import PropTypes from 'prop-types';
-
+import axios from 'axios';
+import Waveform from 'react-audio-waveform';
 import { PlayButton, Progress, Timer } from 'react-soundplayer/components';
-
-const DEFAULT_DURATION = 456.1495; // have to use this become modifying the audio file breaks 2x speed
-const DEFAULT_MP3 = "https://parse-server-ff.s3.amazonaws.com/ae5992f0f5bb1f259bafa41b3771e3bb_call12565815456dwwwwww795896232www-01b59bd3.mp3";
+import { DEFAULT_DURATION, DEFAULT_MP3} from "./constants";
 
 class AudioPlayer extends Component {
     constructor(props) {
@@ -19,10 +18,11 @@ class AudioPlayer extends Component {
     }
 
     seek(secs, play) {
+      console.log(secs, play)
         if (secs && secs.seek != null) secs = secs.seek();
         this.player.seek(secs);
         let toSet = { currentTime: secs };
-        if (play == true) toSet.playing = true;
+        if (play === true) toSet.playing = true;
         this.setState(toSet);
     }
 
@@ -44,13 +44,13 @@ class AudioPlayer extends Component {
             let { mp3url } = this.props;
             if (this.player) {
                 let currentTime = this.player.seek();
-                const duration = mp3url == DEFAULT_MP3 ? DEFAULT_DURATION : this.player.duration();
+                const duration = mp3url === DEFAULT_MP3 ? DEFAULT_DURATION : this.player.duration();
                 const toSet = { currentTime };
                 if (!this.state.duration && duration != null) {
                     toSet.duration = duration;
                 }
                 if (duration != null) toSet.loadErr = false;
-                if (mp3url == DEFAULT_MP3 && currentTime >= DEFAULT_DURATION) {
+                if (mp3url === DEFAULT_MP3 && currentTime >= DEFAULT_DURATION) {
                     this.player.stop();
                     toSet.playing = false;
                     currentTime = 0;
@@ -64,61 +64,91 @@ class AudioPlayer extends Component {
         if (this.playerInterval) clearTimeout(this.playerInterval);
     }
 
-    componentDidMount() {
-        this.getSeek();
+    async componentDidMount() {
+      await this.webAudio();
+      this.getSeek();
     }
 
     isObject(obj) {
         return obj instanceof Object || ((typeof obj === "object") && (obj !== null));
     }
 
+   async webAudio(){
+      const audio = (await axios.get(DEFAULT_MP3, {
+        responseType: 'arraybuffer'
+      })).data;
+      this.setState({ res: new Uint32Array(audio)});
+    }
+
+  mouseOver() {
+    console.log("Mouse over!!!");
+    this.setState({flipped: true});
+  }
+
+  mouseLeave() {
+    console.log("Mouse Leave!!!");
+    this.setState({flipped: false});
+  }
+
     render() {
         const { mp3url } = this.props;
         let { playing, currentTime, duration, speedup, loadErr } = this.state;
         if (this.isObject(currentTime)) currentTime = 0;
-        if (mp3url == DEFAULT_MP3) duration = DEFAULT_DURATION;
+        if (mp3url === DEFAULT_MP3) duration = DEFAULT_DURATION;
         return (
-            <div className="ff-audio">
-                {duration != null ? <div className="flex flex-center px2 relative z1">
-                    <PlayButton
-                        playing={playing}
-                        onTogglePlay={() => this.setState({ playing: !playing })}
-                        className="flex-none h2 mr2 button button-transparent button-grow rounded"
-                    />
-                    {/* seeking={Boolean}
+          <div className="ff-audio">
+              {duration != null ? <div className="flex flex-center px2 relative z1">
+                  <PlayButton
+                    playing={playing}
+                    onTogglePlay={() => this.setState({ playing: !playing })}
+                    className="flex-none h2 mr2 button button-transparent button-grow rounded"
+                  />
+                  {/* seeking={Boolean}
                         seekingIcon={ReactElement} */}
 
-                    <div className="sb-soundplayer-volume mr2 flex flex-center">
-                        <button onClick={() => this.toggleRate()} className="sb-soundplayer-btn sb-soundplayer-volume-btn flex-none h2 button button-transparent button-grow rounded">
-                            <img className={speedup ? 'audio-speedup' : ""} src="/pane/speedup.svg" height={35} />
-                        </button>
-                    </div>
-                    <Progress
-                        className="flex-auto bg-darken-3 rounded"
-                        innerClassName="rounded-left bg-white"
-                        value={((currentTime || 0) / (duration || 1)) * 100 || 0}
-                        onSeekTrack={(ts) => this.seek(ts * duration)}
-                    />
+                  <div className="sb-soundplayer-volume mr2 flex flex-center">
+                      <button onClick={() => this.toggleRate()} className="sb-soundplayer-btn sb-soundplayer-volume-btn flex-none h2 button button-transparent button-grow rounded">
+                          <img className={speedup ? 'audio-speedup' : ""} src="/pane/speedup.svg" height={35} />
+                      </button>
+                  </div>
+                  <div className="flex-auto bg-darken-3 rounded rounded-left"
+                       onDoubleClick={()=> this.mouseOver() }
+                       onMouseOverCapture={()=> this.mouseLeave() }
 
-                    <Timer
-                        className={"timer"}
-                        duration={duration} // in seconds
-                        currentTime={currentTime != null ? currentTime : 0} />
-                </div> : (loadErr ? <div style={{ padding: "5 20px" }}>Unable to load audio: {loadErr}</div> : <div className="progress"><div className="indeterminate" /></div>)}
-                <div>
-                    <ReactHowler
-                        src={mp3url}
-                        playing={playing}
-                        loop={false}
-                        onLoadError={(id, err) => {
-                            console.log('Unable to load media', err);
-                            this.setState({ loadErr: (err && err.message) || 'Startup error' });
-                        }}
-                        onLoad={() => this.getSeek()}
-                        ref={(ref) => (this.player = ref)}
-                    />
-                </div>
-            </div>
+                  >
+                      <Waveform
+                        barWidth={4}
+                        peaks={this.state.res}
+                        height={200}
+                        pos={currentTime}
+                        duration={duration}
+                        onClick={(ts) => this.seek(ts )}
+                        color="#676767"
+                        progressGradientColors={[[0, "#888"], [1, "#aaa"]]}
+                        transitionDuration={300}
+                      />
+                  </div>
+
+
+                  <Timer
+                    className={"timer"}
+                    duration={duration} // in seconds
+                    currentTime={currentTime != null ? currentTime : 0} />
+              </div> : (loadErr ? <div style={{ padding: "5 20px" }}>Unable to load audio: {loadErr}</div> : <div className="progress"><div className="indeterminate" /></div>)}
+              <div>
+                  <ReactHowler
+                    src={mp3url}
+                    playing={playing}
+                    loop={false}
+                    onLoadError={(id, err) => {
+                        console.log('Unable to load media', err);
+                        this.setState({ loadErr: (err && err.message) || 'Startup error' });
+                    }}
+                    onLoad={() => this.getSeek()}
+                    ref={(ref) => (this.player = ref)}
+                  />
+              </div>
+          </div>
         );
     }
 }
---
+++ test/src/constants/index.js
@@ -0,0 +1,2 @@
+export const DEFAULT_DURATION = 456.1495; // have to use this become modifying the audio file breaks 2x speed
+export const DEFAULT_MP3 = "https://parse-server-ff.s3.amazonaws.com/ae5992f0f5bb1f259bafa41b3771e3bb_call12565815456dwwwwww795896232www-01b59bd3.mp3";
